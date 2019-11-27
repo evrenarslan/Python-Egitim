88bfa60a5dc9077ae87495b3310626e61e88cc3b
Evren Arslan <earslan@gmail.com>
2019-11-28 00:02:55 +0300

    Finish dictionary

--- 02_Dictionaries.py
+++ 02_Dictionaries.py
@@ -45,3 +45,29 @@ print(europe)
 del(europe["Polonya"]) # Polonya dictionary içinden silinir.
 
 print(europe)
+
+# Dictionar içinde dictionary
+
+# Dictionary of dictionaries
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data={'capital':'rome','population':59.83}
+
+
+# Add data to europe under key 'italy'
+europe['italy']=data
+
+# Print europe
+print(europe)
+
+
+
